820c3b781f17bb95f67f5e37f016235df789d001
teddy0 <tong011@e.ntu.edu.sg>
2022-06-07 16:02:10 -0700

    added a template test with output helpers

--- tests/pipesAndValves/pipesAndValvesUnitTest.cs
+++ tests/pipesAndValves/pipesAndValvesUnitTest.cs
@@ -1,10 +1,18 @@
 namespace tests;
 
-public class pipesAndValvesUnitTest
+public class pipesAndValvesUnitTest : testOutputHelper
 {
+	public pipesAndValvesUnitTest(ITestOutputHelper outputHelper):base(outputHelper){
+
+		// this constructor is just here to load the test output helper
+		// which is just an object which helps me print code
+		// when i run
+		//' dotnet watch test --logger "console;verbosity=detailed"
+	}
+
     [Fact]
     public void Test1()
     {
-
+		this.cout("hello there");
     }
 }
