48139a3537e172a7b7185303acb291d94a1f34ba
Ted <tong011@e.ntu.edu.sg>
2022-06-09 14:37:02 -0700

    custom resistor vanilla test added in xunit and tested

---
+++ tests/CustomResistor/BaseParameters.cs
@@ -0,0 +1,16 @@
+﻿using SpiceSharp.ParameterSets;
+using SpiceSharp.Attributes;
+
+namespace SpiceSharp.Components.NonlinearResistorBehaviors
+{
+    /// <summary>
+    /// Parameters for a <see cref="NonlinearResistor"/>
+    /// </summary>
+    /// <seealso cref="ParameterSet" />
+    public partial class BaseParameters : ParameterSet<BaseParameters>
+    {
+        public double A { get; set; } = 1.0e3;
+
+        public double B { get; set; } = 1.0;
+    }
+}
--- tests/pipesAndValves/pipesAndValvesUnitTest.cs
+++ tests/pipesAndValves/pipesAndValvesUnitTest.cs
@@ -1,3 +1,7 @@
+using SpiceSharp;
+using SpiceSharp.Components;
+using SpiceSharp.Simulations;
+
 namespace tests;
 
 public class pipesAndValvesUnitTest : testOutputHelper
@@ -15,4 +19,33 @@ public class pipesAndValvesUnitTest : testOutputHelper
     {
 		this.cout("hello there");
     }
+	
+	[Fact]
+	public void When_RunNonlinearResistor_Expect_NoException()
+	{
+		// <example_customcomponent_nonlinearresistor_test>
+		//
+	 NonlinearResistor NLR = new NonlinearResistor("RNL1", "out", "0");
+	 NLR.Parameters.A = 2.0e3;
+	 NLR.Parameters.B = 0.5; 
+
+		// Build the circuit
+		var ckt = new Circuit(
+				new VoltageSource("V1", "out", "0", 0.0),
+				NLR
+				);
+
+		// Setup the simulation and export our current
+		var dc = new DC("DC", "V1", -2.0, 2.0, 1e-2);
+		var currentExport = new RealPropertyExport(dc, "V1", "i");
+		dc.ExportSimulationData += (sender, args) =>
+		{
+			var current = -currentExport.Value;
+			System.Console.Write("{0}, ".FormatString(current));
+		};
+		dc.Run(ckt);
+
+		currentExport.Destroy();
+		// </example_customcomponent_nonlinearresistor_test>
+	}
 }
