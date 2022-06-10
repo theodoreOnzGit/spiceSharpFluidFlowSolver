464378f7b689ea995b1f8eaf03b48e249d2b9331
Ted <tong011@e.ntu.edu.sg>
2022-06-10 16:21:12 -0700

    reference values for rough pipes in turbulent flow added

--- tests/pipesAndValves/pipesAndValvesUnitTest.cs
+++ tests/pipesAndValves/pipesAndValvesUnitTest.cs
@@ -16,6 +16,28 @@ public class pipesAndValvesUnitTest : testOutputHelper
 		//' dotnet watch test --logger "console;verbosity=detailed"
 	}
 
+	// this test will test the churchill correlation over some
+	// values using an online colebrook calculator
+	// https://www.engineeringtoolbox.com/colebrook-equation-d_1031.html
+	// https://www.ajdesigner.com/php_colebrook/colebrook_equation.php#ajscroll
+	// the online calculators return a darcy friction factor
+	[Theory]
+	[InlineData(4000, 0.05, 0.076986834889224)]
+	[InlineData(40000, 0.05, 0.072124054027755)]
+	[InlineData(4e5, 0.05, 0.071608351787938)]
+	[InlineData(4e6, 0.05,  0.071556444535705)]
+	[InlineData(4e7, 0.05,  0.071551250389636)]
+	[InlineData(4e8, 0.05, 0.071550730940769)]
+	[InlineData(4e9, 0.05, 0.071550678995539)]
+	public void Test_churchillFrictionFactorShouldBeAccurate_Turbulent(double Re,double roughnessRatio, double referenceFrictionFactor){
+		// i'm making the variable explicit so the user can see
+		// it's darcy friction factor, no ambiguity here
+		double referenceDarcyFactor = referenceFrictionFactor;
+	}
+
+
+
+
 	[Theory]
 	[InlineData(100, 0.05)]
 	[InlineData(200, 0.05)]
