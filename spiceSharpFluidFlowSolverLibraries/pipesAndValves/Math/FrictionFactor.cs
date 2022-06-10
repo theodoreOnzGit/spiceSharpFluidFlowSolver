dcedd26d898da70bbbedb2837be6375ca55e6243
Ted <tong011@e.ntu.edu.sg>
2022-06-10 12:54:37 -0700

    fanning friction factor function added, yet to be tested

--- spiceSharpFluidFlowSolverLibraries/pipesAndValves/Math/FrictionFactor.cs
+++ spiceSharpFluidFlowSolverLibraries/pipesAndValves/Math/FrictionFactor.cs
@@ -7,19 +7,56 @@ public class ChurchHillFrictionFactor : IFrictionFactor
 {
 	// this particular implementation uses the churchill correlation
 	public double fanning(double ReynoldsNumber, double roughnessRatio){
-		return 0.0;
+
+		double fanningFrictionFactor;
+		fanningFrictionFactor = 2 * Math.Pow(this.churchillInnerTerm(ReynoldsNumber,roughnessRatio), 1/12);
+		return fanningFrictionFactor;
 	}
 
 	public double moody(double ReynoldsNumber, double roughnessRatio){
-		return 0.0;
+
+		// apparently the moody friciton factor is same as the darcy friction factor
+
+		return this.darcy(ReynoldsNumber,roughnessRatio);
 	}
 	
 	public double darcy(double ReynoldsNumber, double roughnessRatio){
-		return 0.0;
+
+		// darcy friction factor is 4x fanning friction factor
+		// https://neutrium.net/fluid-flow/pressure-loss-in-pipe/
+		double darcyFrictionFactor;
+		darcyFrictionFactor = 4 * this.fanning(ReynoldsNumber,roughnessRatio);
+		return darcyFrictionFactor;
+	}
+
+
+
+	private double churchillInnerTerm(double Re, double roughnessRatio){
+
+		double laminarTerm;
+		laminarTerm = Math.Pow(8/Re, 12);
+
+		double turbulentTerm;
+		double Aterm = this.A(Re,roughnessRatio);
+		double Bterm = this.B(Re);
+
+		turbulentTerm = Math.Pow( 1/(Aterm + Bterm), 3/2);
+
+		return laminarTerm + turbulentTerm;
+
+
 	}
 
 	private double A(double Re, double roughnessRatio){
-		return 0.0;
+		// first i need the logarithm of a number
+
+		double reynoldsTerm =  Math.Pow(7/Re , 0.9);
+		double roughnessTerm = 0.27*roughnessRatio;
+
+		double logFraction = Math.Pow(reynoldsTerm+roughnessTerm,-1);
+		double A = 2.457*Math.Log(logFraction);
+		
+		return A;
 	}
 
 	private double B(double Re){
