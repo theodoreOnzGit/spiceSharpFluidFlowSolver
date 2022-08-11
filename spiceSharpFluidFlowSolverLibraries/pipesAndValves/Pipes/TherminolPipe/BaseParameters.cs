d8369690a55d302038a3254c79e6f6ee224f2371
teddy0@archIdeaPadDesktop <tong011@e.ntu.edu.sg>
2022-08-11 14:32:55 -0700

    therminol pipe template class constructed

---
+++ spiceSharpFluidFlowSolverLibraries/pipesAndValves/Pipes/TherminolPipe/BaseParameters.cs
@@ -0,0 +1,675 @@
+﻿using SpiceSharp.ParameterSets;
+using SpiceSharp.Attributes;
+using EngineeringUnits;
+using EngineeringUnits.Units;
+using MathNet.Numerics;
+using MathNet.Numerics.Interpolation;
+
+namespace SpiceSharp.Components.TherminolPipeBehaviors
+{
+    public partial class BaseParameters : 
+		ParameterSet<BaseParameters>
+    {
+
+		// now we have the dimensioned units for pipe
+		// this is assumed to be circular
+
+		
+		// now i want to make it such that the interpolation is redone
+		// anytime the L/D changes, K changes 
+		// or surface roughness changes
+		private Length _hydraulicDiameter = new Length(1.0,LengthUnit.Meter);
+
+		public Length hydraulicDiameter { 
+		   get {
+			   return this._hydraulicDiameter;
+		   }	   
+		   set{ 
+			  this._hydraulicDiameter = value;
+			 this.constructInterpolateReFromBe(); 
+		   }
+		}  
+
+		// now i want to make it such that the interpolation is redone
+		// anytime the L/D changes, K changes 
+		// or surface roughness changes
+
+		private Length _pipeLength = new Length(10.0,LengthUnit.Meter);
+		public Length pipeLength { 
+			get {
+				return this._pipeLength;
+			}
+			set {
+				this._pipeLength = value; 
+				this.constructInterpolateReFromBe();
+			}
+		} 
+
+		// next we also have angles as well
+		// this is mainly to calculate hydrostatic pressure increase or
+		// decrease
+
+		public Angle inclineAngle { get; set; } =
+			new Angle(0.0, AngleUnit.Degree);
+
+		public Pressure hydrostaticPressureChange(){
+
+			// this is the change in hydrostatic pressure 
+			// ie pressure of pipe exit - pressure of pipe entrance
+			Pressure hydrostaticPressureChange;
+
+
+			hydrostaticPressureChange = 
+				this.hydrostaticKinematicPressureChange()*
+				this.fluidDesnity();
+
+			hydrostaticPressureChange = 
+				hydrostaticPressureChange.ToUnit(
+						PressureUnit.Pascal);
+
+			return hydrostaticPressureChange;
+		}
+
+		public SpecificEnergy hydrostaticKinematicPressureChange(){
+
+			// this is the change in hydrostatic pressure 
+			// ie pressure of pipe exit - pressure of pipe entrance
+			SpecificEnergy hydrostaticKinematicPressureChange;
+
+			Length heightChange =
+				this.pipeLength * Math.Sin(this.inclineAngle.As(
+							AngleUnit.Radian));
+
+			// for gravity let me initiate a constants class
+
+			Acceleration _standardGravity =
+				new Acceleration(9.81, AccelerationUnit.MeterPerSecondSquared);
+
+			// now kinematic pressure change is just gz
+
+			hydrostaticKinematicPressureChange = 
+				heightChange * _standardGravity;
+
+			hydrostaticKinematicPressureChange = 
+				hydrostaticKinematicPressureChange.ToUnit(
+						SpecificEnergyUnit.JoulePerKilogram);
+
+			return hydrostaticKinematicPressureChange;
+
+
+		}
+		
+
+		// carbon steel surface roughness used as default
+		public Length _absoluteRoughness = 
+			new Length (0.15, LengthUnit.Millimeter);
+		public Length absoluteRoughness { 
+			get{
+				return this._absoluteRoughness;
+			}
+			set{
+				this._absoluteRoughness = value;
+				this.constructInterpolateReFromBe();
+			}
+		} 
+
+		// derived quantites and ratios
+
+		public Area crossSectionalArea(){
+			Area finalResult;
+			finalResult = this.hydraulicDiameter.Pow(2)/4*Math.PI;
+			return finalResult;
+		}
+
+		public double roughnessRatio(){
+			return absoluteRoughness.As(LengthUnit.Meter)/
+				hydraulicDiameter.As(LengthUnit.Meter);
+		}
+
+		public double lengthToDiameter(){
+			return pipeLength.As(LengthUnit.Meter)/
+				hydraulicDiameter.As(LengthUnit.Meter);
+		}
+		
+
+
+		// and also fluid properties
+		// water at 18C used as default
+		// https://www.engineeringtoolbox.com/water-dynamic-kinematic-viscosity-d_596.html
+
+		public KinematicViscosity fluidKinViscosity { get; set; } =
+			new KinematicViscosity(1.0533, KinematicViscosityUnit.Centistokes);
+
+		public DynamicViscosity fluidViscosity { get; set; } =
+			new DynamicViscosity(1.0518, DynamicViscosityUnit.Centipoise);
+
+		public Density fluidDesnity(){
+			// i find the density here by comparing
+			// kinematic viscosity to dynamic viscosity ratios
+			// mu = rho* nu
+			// rho = mu/nu
+
+			DynamicViscosity fluidViscosity = 
+				this.fluidViscosity;
+			
+			fluidViscosity = fluidViscosity.ToUnit(DynamicViscosityUnit.
+					PascalSecond);
+
+			KinematicViscosity fluidKinViscosity =
+				this.fluidKinViscosity;
+
+			fluidKinViscosity = fluidKinViscosity.ToUnit(KinematicViscosityUnit.
+					SquareMeterPerSecond);
+
+			Density fluidDesnity = fluidViscosity/fluidKinViscosity;
+			fluidDesnity = fluidDesnity.ToUnit(
+					DensityUnit.KilogramPerCubicMeter);
+
+			return fluidDesnity;
+		}
+
+		// constructors
+		//
+
+		public BaseParameters(){
+			// this constructor is here to construct interpolation objects
+			this.constructInterpolateReFromBe();
+		}
+
+
+		IInterpolation _interpolateReFromBe;
+
+		public void constructInterpolateReFromBe(){
+			// first let me get a set of about 1000 values
+			//
+
+			IList<double> ReValues = new List<double>();
+			IList<double> BeValues = new List<double>();
+			// at Re = 0, Be also = 0
+			// this is the first point
+
+			ReValues.Add(0.0);
+			BeValues.Add(0.0);
+			for (int i = 0; i < 500; i++)
+			{
+				// first i decide on a number of values to give
+				double ReLogSpacing = 0.02;
+				double ReValue = Math.Pow(10,ReLogSpacing * i);
+				ReValues.Add(ReValue);
+
+				// then i decide that okay, let me get my Bejan numbers
+				// from Re
+				// Be_D = 0.5 * (f_darcy * L/D + K) Re^2
+				// here, we don't exactly consider K just yet
+				// so we just use
+				//
+				// Be_D = 0.5* (f_darcy* L/D) Re^2,
+				// what we need here though is L/D and roughness ratio
+				// besides Re
+				double bejanNumber;
+				double darcyFrictionFactorReSq =
+					this.constantRoughnessFanningReSq(ReValue)
+					*4.0;
+				bejanNumber = 0.5* darcyFrictionFactorReSq *
+					this.lengthToDiameter();
+				BeValues.Add(bejanNumber);
+			}
+
+			this._interpolateReFromBe = Interpolate.CubicSpline(
+					BeValues,ReValues);
+
+		}
+
+		//
+		// i have changed this to a function returning new instances
+		// of StabilisedChurchillJacobian in order to avoid race
+		// conditions
+		public IFrictionFactorJacobian jacobianObject(){
+			return new StabilisedChurchillJacobian();
+		}
+		
+		// we can get dynamic pressure drop from mass flowrate as follows
+		// (1) convert mass flowrate to Reynolds number
+		// (2) convert Reynolds number to Bejan number
+		// (3) convert Bejan number back to pressure drop
+
+
+		public double getReynoldsNumber(MassFlow massFlowrate){
+			// the reynold's number here is based off mass flowrate
+			// massflowrate/XSArea * hydraulicDiameter / dynamicViscosity
+			double ReynoldsNumber;
+			ReynoldsNumber = massFlowrate.As(MassFlowUnit.KilogramPerSecond);
+			ReynoldsNumber /= this.crossSectionalArea().As(AreaUnit.SquareMeter);
+			ReynoldsNumber *= this.hydraulicDiameter.As(LengthUnit.Meter);
+			ReynoldsNumber /= this.fluidViscosity.As(
+					DynamicViscosityUnit.PascalSecond);
+
+			return ReynoldsNumber;
+		}
+
+		// this function is here to return a Bejan number
+		// this is based on Be_D not Be_L
+		// ie Be = deltaP * D^2/nu^2
+		// i use the formula
+		// (f_darcy L/D + K )Re^2 = 2 Be_D
+		// i assume K is exactly zero here
+		// so no need to include that
+
+		public double getBejanNumber(MassFlow massFlowrate){
+
+			// now i want to develop a way to deal with negative numbers
+			// by default, isNegativeMassFlow is false
+			bool isNegativeMassFlow = false;
+
+			if (massFlowrate.As(MassFlowUnit.KilogramPerSecond) < 0.0)
+			{
+				// if mass flowrate is less than 0,
+				// change the boolean isNegativeMassFlow to true
+				// and make the massFlowrate positive
+				isNegativeMassFlow = true;
+				massFlowrate *= -1.0;
+			}
+
+			double Re = this.getReynoldsNumber(massFlowrate);
+
+			double darcyFrictionFactorReSq =
+				this.constantRoughnessFanningReSq(Re)
+				*4.0;
+
+			double BejanNumber;
+			BejanNumber = 0.5;
+			BejanNumber *= darcyFrictionFactorReSq;
+			BejanNumber *= this.lengthToDiameter();
+
+			if(isNegativeMassFlow){
+				// if i have a negative mass flowrate value,
+				// i will return a negative Bejan Number
+				return BejanNumber * -1.0;
+			}
+			return BejanNumber;
+
+		}
+
+		private double constantRoughnessFanningReSq(double Re){
+
+			// now the graph in the negative x direction should be a 
+			// reflection of the graph in positive x direction along
+			// the y axis. So this is using absolute value of Re
+
+			
+			Re = Math.Abs(Re);
+
+			// the fanning friction factor function (this.fanning)
+			// can return Re values for various values in turbulent as
+			// well as laminar region
+			//
+			// However, if Re is close to zero, 
+			// the function is not well behaved
+			//
+			// since we are returning f*Re^2
+			// we can use the laminar region fanning friction factor
+			// which is 16/Re
+			// for lower Re eg. Re<1
+			// However, we also note that it's quite computationally cheap
+			// in that you only need to perform one calculation
+			// Hence, it's quite advantageous to let it take more Reynold's 
+			// numbers
+			// so for most of the laminar regime, it is good to use the 
+			// 16/Re formula
+			//
+			// We should note however that for a piecewise function
+			// there is some discontinuity between the two functions
+			// ie the churchill and the Pousille function
+			//
+			// While this is a concern, let's ignore it for now
+			// and fix the problem if it crops up.
+			//
+			// So if Re>1800 we return the traditional fanning formula
+
+			double transitionPoint = 1800.0;
+
+			if (Re > transitionPoint)
+			{
+				double fanningReSq = jacobianObject().fanning(
+						Re, this.roughnessRatio())*
+					Math.Pow(Re,2.0);
+
+				return fanningReSq;
+			}
+
+			// otherwise we return 16/Re*Re^2 or 16*Re
+			//
+			IInterpolation _linear;
+
+			IList<double> xValues = new List<double>();
+			IList<double> yValues = new List<double>();
+			xValues.Add(0.0);
+			xValues.Add(transitionPoint);
+
+			yValues.Add(0.0);
+			yValues.Add(jacobianObject().fanning(transitionPoint,
+						this.roughnessRatio())*
+					Math.Pow(transitionPoint,2.0));
+
+			_linear = Interpolate.Linear(xValues,yValues);
+
+
+			return _linear.Interpolate(Re);
+			
+			// my only concern here is a potential problem if the root is exactly at
+			// Re = 1800
+		}
+
+		// this is a function to get the pressure drop
+		// given a mass flowrate
+		// we use the formula
+		// Be_D = Delta P * D^2 / (mu*nu)
+		// we don't include hydrostatic pressure changes here
+		public Pressure getPressureDrop(MassFlow massFlowrate){
+
+			double BejanNumber = this.getBejanNumber(massFlowrate);
+
+			KinematicViscosity fluidKinViscosity;
+			fluidKinViscosity = this.fluidKinViscosity.ToUnit(
+					KinematicViscosityUnit.SquareMeterPerSecond);
+
+			DynamicViscosity fluidViscosity;
+			fluidViscosity = this.fluidViscosity.ToUnit(
+					DynamicViscosityUnit.PascalSecond);
+
+			Pressure pressureDrop = fluidKinViscosity *
+				fluidViscosity /
+				this.hydraulicDiameter /
+				this.hydraulicDiameter;
+
+			pressureDrop *= BejanNumber;
+			pressureDrop = pressureDrop.ToUnit(
+					PressureUnit.Pascal);
+
+
+			return pressureDrop;
+			
+
+		}
+
+		public SpecificEnergy getKinematicPressureDrop(
+				MassFlow massFlowrate){
+
+			double BejanNumber = this.getBejanNumber(massFlowrate);
+			
+			KinematicViscosity fluidKinViscosity;
+			fluidKinViscosity = this.fluidKinViscosity.ToUnit(
+					KinematicViscosityUnit.SquareMeterPerSecond);
+
+			SpecificEnergy pressureDrop = fluidKinViscosity *
+				fluidKinViscosity /
+				this.hydraulicDiameter /
+				this.hydraulicDiameter;
+
+			pressureDrop *= BejanNumber;
+			pressureDrop = pressureDrop.ToUnit(
+					SpecificEnergyUnit.JoulePerKilogram);
+
+			return pressureDrop;
+			
+
+		}
+
+		// here are methods to get MassFlowrate
+		//
+
+		public MassFlow getMassFlowRate(SpecificEnergy kinematicPressureDrop){
+			// this supposes that a kinematicPressureDrop is supplied over this pipe
+			// and we want to get the mass flowrate straightaway.
+			//
+
+			// note: it's not thread safe to run multiple instances
+			// of getMassFlowrate since i will be manipulating variables multiple
+			// times
+			
+			// first let's define a function to help us
+			// get a double of kinematicPressureDrop from
+			// the double of massFlowrate
+			// numbers must be in double format so as to
+			// make it compatible with MathNet FindRoots.OfFunction
+			// rootfinder
+			//
+			// I think an upper value of 1e12 kg/s will suffice
+			//
+			// so this function will take in a massFlowrate
+			// and iterate till the desired kinematic pressure dorp is achieved
+			//
+		    
+			// before i start calculations, i will need to adjust for hydrostatic pressure
+			//
+			kinematicPressureDrop -= this.hydrostaticKinematicPressureChange();
+			// first i'll need to see if this value is negative 
+
+			bool isNegativePressure = false;
+
+			if (kinematicPressureDrop.As(SpecificEnergyUnit.SI) < 0.0)
+			{
+				isNegativePressure = true;
+				kinematicPressureDrop *= -1.0;
+			}
+			// next i want to see if pressure drop is zero
+			// i will automatically just return mass flowrate of zero
+
+			if (kinematicPressureDrop.As(SpecificEnergyUnit.SI) == 0.0){
+				return new MassFlow(0.0, MassFlowUnit.KilogramPerSecond);
+			}
+			// now if the Bejan number is low enough, we can just use
+			// interpolation done from the constructor objects
+			// let's get Bejan number first
+
+
+			double getBejanFromKinematicPressureDrop(SpecificEnergy 
+					kinematicPressureDrop){
+				// Be_D = kinPressureDrop * D^2/nu^2
+				// D = hydraulicDiameter
+				// nu = kinematicViscosity
+				double Be_D;
+
+				double nuSqared = Math.Pow(this.fluidKinViscosity.As( 
+							KinematicViscosityUnit.SquareMeterPerSecond)
+						,2.0);
+
+				double Dsquared = Math.Pow(this.hydraulicDiameter.As(
+							LengthUnit.Meter)
+						,2.0);
+
+
+				Be_D = kinematicPressureDrop.As(SpecificEnergyUnit.
+						JoulePerKilogram) * Dsquared / nuSqared;
+
+				return Be_D;
+
+			}
+
+			double Be_D;
+			Be_D = getBejanFromKinematicPressureDrop(kinematicPressureDrop);
+
+			if(Be_D < 1e10){
+				// if Be_D is sufficiently small, within our interpolation range
+				// we can interpolate it rather than go about iterating our
+				// way to an answer
+				double Re;
+				Re = this._interpolateReFromBe.Interpolate(Be_D);
+
+
+				MassFlow massFlowrateFromRe(double Re){
+
+					// Re = massflow/
+					MassFlow flowrate =
+						this.crossSectionalArea()/
+						this.hydraulicDiameter*
+						this.fluidViscosity*
+						Re;
+
+					return flowrate.ToUnit(MassFlowUnit.
+							KilogramPerSecond);
+
+				}
+
+				return massFlowrateFromRe(Re);
+				
+			}
+
+			this.kinematicPressureDropValJoulePerKg =
+				kinematicPressureDrop.As(SpecificEnergyUnit.
+						JoulePerKilogram);
+
+			double pressureDropRoot(double massFlowValueKgPerS){
+
+				// so i have a reference kinematic presureDrop value
+				double kinematicPressureDropValJoulePerKg = 
+					this.kinematicPressureDropValJoulePerKg;
+
+				// and then i also have a iterated pressureDrop value
+
+				double iteratedPressureDropValJoulePerKg;
+
+				MassFlow massFlowrate;
+				massFlowrate = new MassFlow(massFlowValueKgPerS,
+						MassFlowUnit.KilogramPerSecond);
+
+
+				SpecificEnergy kinematicPressureDrop;
+				kinematicPressureDrop = 
+					this.getKinematicPressureDrop(massFlowrate);
+
+				iteratedPressureDropValJoulePerKg =
+					kinematicPressureDrop.As(SpecificEnergyUnit.
+							JoulePerKilogram);
+
+				// so this is the function, to have iterated pressure drop
+				// equal the kinematic pressure drop,
+				// set the value to zero
+				double functionValue =
+					iteratedPressureDropValJoulePerKg -
+					kinematicPressureDropValJoulePerKg;
+
+
+				return functionValue;
+			}
+
+			// here i use MathNet's FindRoots function to get the mass flowrate
+			double massFlowValueKgPerS;
+			massFlowValueKgPerS = FindRoots.OfFunction(pressureDropRoot,
+					-1e12,1e12);
+
+			MassFlow massFlowrate;
+			massFlowrate = new MassFlow(massFlowValueKgPerS,
+					MassFlowUnit.KilogramPerSecond);
+
+			// after i'm done, do some cleanup operations
+			this.kinematicPressureDropValJoulePerKg = 0.0;
+
+
+			// and finally let me return the mass flowrate
+				if (isNegativePressure)
+				{
+					return massFlowrate * -1.0;
+				}
+			return massFlowrate;
+
+		
+		}
+
+		public double kinematicPressureDropValJoulePerKg = 0.0;
+
+		public MassFlow getMassFlowRate(Pressure dynamicPressureDrop){
+			// i'm just going to recycle the kinematic pressure drop
+			// code
+			// by turning converting dynamic pressure drop into
+			// kinematic pressure drop first
+
+			SpecificEnergy kinematicPressureDrop = 
+				dynamicPressureDrop/this.fluidDesnity();
+
+			return this.getMassFlowRate(kinematicPressureDrop);
+
+
+		}
+
+		public MassFlow getMassFlowRateUsingIteration(Pressure dynamicPressureDrop)
+		{
+			// before we start anything
+			// we adjust for hydrostatic pressure
+
+			dynamicPressureDrop -= this.hydrostaticPressureChange();
+			// first let's check for negative pressure values
+			// ie reverse direction
+			bool isNegativePressure = false;
+
+			if (dynamicPressureDrop.As(PressureUnit.SI) < 0.0)
+			{
+				isNegativePressure = true;
+				dynamicPressureDrop *= -1.0;
+			}
+
+			// next i want to see if pressure drop is zero
+			// i will automatically just return mass flowrate of zero
+
+			if (dynamicPressureDrop.As(PressureUnit.SI) == 0.0){
+				return new MassFlow(0.0, MassFlowUnit.KilogramPerSecond);
+			}
+
+			this.dynamicPressureDropValuePascal = 
+				dynamicPressureDrop.As(PressureUnit.
+						Pascal);
+
+			double pressureDropRoot(double massFlowValueKgPerS){
+
+				double dynamicPressureDropValuePascal 
+					= this.dynamicPressureDropValuePascal;
+
+				// let's do the iterated pressureDropValue
+				double iteratedPressureDropValPascal;
+
+				// so i'll have a mass flowrate
+				// and iterate a pressure drop out
+				MassFlow massFlowrate;
+				massFlowrate = new MassFlow(massFlowValueKgPerS,
+						MassFlowUnit.KilogramPerSecond);
+
+
+				Pressure pressureDrop 
+					= this.getPressureDrop(massFlowrate);
+
+				pressureDrop = pressureDrop.ToUnit(
+						PressureUnit.Pascal);
+
+				iteratedPressureDropValPascal =
+					pressureDrop.As(PressureUnit.Pascal);
+
+				double functionValue =
+					iteratedPressureDropValPascal -
+					dynamicPressureDropValuePascal;
+
+				return functionValue;
+			}
+
+			double massFlowValueKgPerS;
+			massFlowValueKgPerS = FindRoots.OfFunction(pressureDropRoot,
+					-1e12,1e12);
+
+			MassFlow massFlowrate;
+			massFlowrate = new MassFlow(massFlowValueKgPerS,
+					MassFlowUnit.KilogramPerSecond);
+			// after i'm done, do some cleanup operations
+			this.dynamicPressureDropValuePascal = 0.0;
+
+
+			// and finally let me return the mass flowrate
+				if (isNegativePressure)
+				{
+					return massFlowrate * -1.0;
+				}
+			return massFlowrate;
+		}
+
+		public double dynamicPressureDropValuePascal = 0.0;
+
+    }
+}
---
+++ spiceSharpFluidFlowSolverLibraries/pipesAndValves/Pipes/TherminolPipe/README.md
@@ -0,0 +1,100 @@
+# Therminol Pipe
+
+The therminol pipe class is a pipe which will form the foundation
+of all therminol based components
+
+$$Be_D = 0.5 Re_D^2 (f_{darcy} \frac{L}{D} + K)$$
+
+The pipe will form the basis of all therminol based components.
+
+The only things the user must define are
+
+1. Fluid Temperature (from this all fluid properties are calculated)
+2. Pipe dimensions
+3. fLDK factors
+
+This itself is an abstract class from which all therminol type pipes are
+derived.
+
+## Inner Nodalisation
+
+For a first iteration of the pipe, only ONE temperature is defined.
+
+In reality however, especially for longer pipes, the temperature will 
+vary throughout the pipe. We will therefore need nodes to calculate the
+temperature distribution. However, adding several nodes in this pipe
+may add additional unnecessary computational burden to this class.
+
+If we were to introduce nodalisation eg. with 10 nodes , 
+then we will need to break this pipe into ten subcomponents. 
+To ensure that computational expediency is achieved, the same fLDK
+factor is just divided by 10 for a given mass flowrate.
+
+Given that hydraulic diameter is the same, the only things that may 
+change are viscosity and density of the fluid.
+
+We would then store a list of temperatures at different points of the 
+pipe. These temperatures will be used to retrieve the fluid properties
+of the pipe. For which the same fLDK equation is used to calculate 
+pressure losses.
+
+Doing so, we can speed up calculations still using the precalculation 
+technique.
+
+## Interfaces
+
+### IFluidEntity
+The therminol pipe should fulfil the IFluidEntity interface, where 
+pressure losses can be obtained from mass flow and vice versa.
+
+In fact, a list of functions should be standardised to help us return 
+these fluid quantities
+
+1. pressure loss from mass flowrate
+2. mass flowrate from pressure loss
+3. fLDK factor given a pressure loss, or mass flowrate
+4. Property: List of temperatures for flow calculations
+5. accessing component length
+6. Accessing component diameters
+7. accessing area
+8. accessing fluid density
+9. accessing fluid kinematic viscosity
+10. accessing fluid dynamic viscosity
+11. accessing hydrostatic pressure change
+12. accessing change in elevation z
+13. accessing change in coordinates (dx,dy,dz) --> important for 
+fluidParallelCircuit, where i want to ensure all the branches
+end up in the same location
+14. Access Bejan number given pressure loss/drop
+15. Access Reynolds number given mass flowrate
+16. obtain mass flowrate from Re
+17. obtain Bejan number from Re
+
+sounds like Be and Re should be more like properties with different
+get and set functions. But i'd prefer a functional style of programming
+to avoid calculations being dependent on object state as far as possible.
+
+
+## Definition of Pipe via inheriting from Abstract class
+
+The abstract class of the pipe would define how the 
+interpolation objects would be done in the base constructor.
+
+Note that the base constructor must be called, otherwise the 
+interpolation won't work.
+
+### Common methods
+
+TBC
+
+
+## Tests
+
+
+
+# Bibliography
+
+<a id="frictionFactorApproximations">
+[1]
+Zeyu, Z., Junrui, C., Zhanbin, L., Zengguang, X., & Peng, L. (2020). Approximations of the Darcy–Weisbach friction factor in a vertical pipe with full flow regime. Water Supply, 20(4), 1321-1333.
+</a>
