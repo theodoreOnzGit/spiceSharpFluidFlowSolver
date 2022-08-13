52d1e5476189580a4b8e3bdbf71bd782bc4ff14b
teddy0@archIdeaPadDesktop <tong011@e.ntu.edu.sg>
2022-08-12 19:56:23 -0700

    linuxCoolProp not working, SharpFluids not working on linux, windows ok

---
+++ linuxCoolPropTest/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SharpFluids;
+using EngineeringUnits;
+using EngineeringUnits.Units;
+using System;
+
+// make a new therminol-VP1 fluid object
+
+Fluid therminol = new Fluid(FluidList.InCompTherminolVP1);
+
+// set temperature and pressure
+
+Pressure atmosphericPressure = new Pressure(1.1013e5, PressureUnit.Pascal);
+EngineeringUnits.Temperature roomTemperature 
+= new EngineeringUnits.Temperature(293, TemperatureUnit.Kelvin);
+
+// update PT of therminol
+// updates the temperature and pressure of therminol
+
+therminol.UpdatePT(atmosphericPressure, roomTemperature);
+
+// obtain prandtl number
+Console.WriteLine(therminol.Prandtl);
